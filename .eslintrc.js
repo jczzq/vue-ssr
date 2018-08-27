51c81223c7b3d479dddcbefb6915700bee9cebec
jczzq <1195262383@qq.com>
2018-08-27 16:05:27 +0800

    update: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,31 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-      // 函数定义时括号前的空格
-      "space-before-function-paren": [0, {"anonymous": "always", "named": "never"}]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // 4个空格缩进
+    indent: ['error', 4, { SwitchCase: 1 }],
+    // 分号
+    semi: ['error', 'always'],
+    // 形参可定义不使用
+    'no-unused-vars': [
+        'error',
+        { vars: 'all', args: 'none', ignoreRestSiblings: false }
+    ],
+    // 函数声明
+    'space-before-function-paren': [
+        'error',
+        { anonymous: 'never', named: 'never', asyncArrow: 'always' }
+    ],
+    'no-new': 0,
+    'standard/computed-property-even-spacing': [2, 'never'],
+    'standard/no-callback-literal': [2, 'never'],
+    'prefer-promise-reject-errors': 'off',
+    'import/first': 'off',
+    'no-useless-return': 'off'
   },
-  globals: {
-    M: true,
-    flvjs: true
-  }
+  globals: {}
 }
